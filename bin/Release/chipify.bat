e43fb1386f0153a7b96a89e5b2c30a85cd83d5e0
Fragmare <fragmare@yahoo.com>
2017-02-27 01:19:56 -0600

    Update chipify.bat

--- bin/Release/chipify.bat
+++ bin/Release/chipify.bat
@@ -1,10 +1,19 @@
-rem cleanup from before
+SET /P CHIPRATE=Enter new sample rate in Hz: 
+IF %CHIPRATE% EQU 0 GOTO :END
+
+SET /A UserInputVal="%CHIPRATE%"*1
+IF %UserInputVal% EQU 0 GOTO END
+
 del %1.*.dmw
-del %1.small.wav
-del %1.small.wav.txt
+del %1.CHIPIFIED.wav
+del %1.CHIPIFIED.wav.txt
 
-rem decrease volume to a range [16,15] and resample while we're at it
-sox -D -v 0.00048829615 %1 -r 1860 %1.small.wav
+sox -D %1 -c 1 -r "%CHIPRATE%" "%~1.TMP1.wav"
+sox -D "%~1.TMP1.wav" "%~1.TMP2.wav" norm
+sox -D -v 0.00048829615 "%~1.TMP2.wav" "%~1.CHIPIFIED.wav"
 
-rem produces myfile.wav.small.wav.0.dmw, etc. with the chip samples you need
-chipifier %1.small.wav
+chipifier "%~1.CHIPIFIED.wav"
+
+del %1.TMP*.wav
+
+:END
